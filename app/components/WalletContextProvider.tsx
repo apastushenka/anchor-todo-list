e141389974510d81e6703dc81090c2d5d0703769
Alexander Pastushenka <a.pastushenka@gmail.com>
2022-09-07 12:09:33 +0400

    Add Solflare wallet support

--- app/components/WalletContextProvider.tsx
+++ app/components/WalletContextProvider.tsx
@@ -1,13 +1,16 @@
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { FC, ReactNode } from 'react';
 
 require('@solana/wallet-adapter-react-ui/styles.css')
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const endpoint = 'http://localhost:8899'
-    const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
+    const wallets = [
+        new PhantomWalletAdapter(),
+        new SolflareWalletAdapter(),
+    ]
 
     return (
         <ConnectionProvider endpoint={endpoint}>
